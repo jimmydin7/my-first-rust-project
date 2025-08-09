4f62c059c7981fffebcb432f00a1bff257bf433f
Jim Dinias <129774378+jimmydin7@users.noreply.github.com>
2025-08-09 21:03:48 +0300

    add function for calculator!!!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::io::{self,Write};
 
 
+fn calculator(num1:i8, num2:i8) -> i8{
+    return num1 + num2;
+}
+
 fn main(){
 
     let mut name = String::new();
@@ -14,30 +18,27 @@ fn main(){
     println!("---------------------");
 
 
+    // calculator!!
     
     let mut num1 = String::new();
     let mut num2 = String::new();
 
-
-    
-
-
-
     print!("Enter num1 > ");
-    io::stdout().flush();
+    io::stdout().flush().unwrap();
     io::stdin().read_line(&mut num1).unwrap();
 
     print!("Enter num2 > ");
-    io::stdout().flush();
+    io::stdout().flush().unwrap();
     io::stdin().read_line(&mut num2).unwrap();
 
     // 8 bits
     let num1: i8 = num1.trim().parse().expect("msg");
     let num2: i8 = num2.trim().parse().expect("msg");
 
-    let output = num1 + num2;
+    let output = calculator(num1, num2);
 
     println!("{} + {} = {}", num1, num2, output);
 
 
 }
+
