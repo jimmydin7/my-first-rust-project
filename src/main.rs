891fb11963e2cf7a172927113462640ba8528548
Jim Dinias <129774378+jimmydin7@users.noreply.github.com>
2025-08-09 20:37:20 +0300

    add calculator logic

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,28 @@ fn main(){
     println!("Hello {}!", name);
 
 
+    
+    let mut num1 = String::new();
+    let mut num2 = String::new();
+
+
+    
+
+
+
+    println!("Enter num1 > ");
+    io::stdin().read_line(&mut num1).unwrap();
+
+    println!("Enter num2 > ");
+    io::stdin().read_line(&mut num2).unwrap();
+
+    // 8 bits
+    let num1: i8 = num1.trim().parse().expect("msg");
+    let num2: i8 = num2.trim().parse().expect("msg");
+
+    let mut output = num1 + num2;
+
+    println!("{} + {} = {}", num1, num2, output);
+
+
 }
